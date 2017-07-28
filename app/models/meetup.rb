6a905e6fe319cd81a5a94eb3c10d7e60e88426db
klpk011 <klpk011@klpk011.local>
2017-07-29 00:23:53 +0800

    implement meetup model test

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -2,4 +2,12 @@ class Meetup < ApplicationRecord
   validates :title, presence: true
   belongs_to :user
   has_many :comments
+
+  def self.no_description
+     where(:description => nil)
+   end
+
+   def abstract
+     self.description[0..20]
+   end
 end
