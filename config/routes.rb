fce8d649bf08fb3841821477945fd1be8d77d1c0
klpk011 <klpk011@klpk011.local>
2017-07-28 16:42:54 +0800

    add api base and user controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
     resources :comments
   end
   root 'meetups#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :create, :update, :destroy]
+    end
+  end
 end
